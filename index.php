3a6186309a88e99721707bae46ee35df89c57ab8
Ramprakash Baunia <103239547+ramprakashbaunia@users.noreply.github.com>
2025-07-21 12:42:31 +0530

    all folder added

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>MY name is Ramprakash Baunia</h1>
+</body>
+</html>
